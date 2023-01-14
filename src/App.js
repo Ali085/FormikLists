3379d2a0689d32f104c88ee69da336c688b7c5bd
unknown <ali085>
2023-01-14 02:16:58 -0800

    list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {Formik, Field, Form } from "formik"
+import React,{ useState } from 'react';
 
 function App() {
+  const [List, setList] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='MyForm'>
+      <Formik initialValues={{
+        username:"",
+        password:"",
+        email:"",
+        about:"",
+        phoneNumber:"",
+      }}
+      onSubmit={(values)=>{
+        setList((prevState) => [...prevState,values])
+      }}
+      >
+        {(props)=>(
+
+          <Form>
+            Username
+            <Field name="username" />
+            Password
+            <Field name="password" type="password"/>
+            Email
+            <Field name="email" />
+            About
+            <Field name="about" />
+            Phone Number
+            <Field name="phoneNumber" />
+            <button type='submit'>Sign in</button>
+          </Form>
+        )}
+      </Formik>
+          {
+            <ol>
+              {
+                List.map((e,i) => {
+                  return (
+                    <li key={i}>
+                      <p>{e.username}</p>
+                      <p>{e.password}</p>
+                      <p>{e.email}</p>
+                      <p>{e.about}</p>
+                      <p>{e.phoneNumber}</p>
+                    </li>
+                  )
+                })
+              }
+            </ol>
+          }
     </div>
-  );
+    )
 }
 
 export default App;
